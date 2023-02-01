a7ab078f2f499b80945a977bdd38d768cbd4282b
rafaellimasnp <rafaelrock722@gmail.com>
2023-01-31 22:15:35 -0400

    correção pacote Contas

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	c "https://github.com/rafaellimasnp/curso-golang-alura-oop"
 )
 
 func main() {
-	contaRafael := Contas.ContaCorrente{"Rafael Lima", 1189, 1234, 1000}
-	contaMitali := Contas.ContaCorrente{"MItali", 1189, 1234, 1000}
+	contaRafael := c.ContaCorrente{"Rafael Lima", 1189, 1234, 1000}
+	contaMitali := c.ContaCorrente{"MItali", 1189, 1234, 1000}
 
 	status := contaRafael.Transferir(300, &contaMitali)
 
