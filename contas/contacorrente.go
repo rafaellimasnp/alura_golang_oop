a094285f33709bfd24d3c88186b3a343716a5e44
rafaellimasnp <rafaelrock722@gmail.com>
2023-01-31 21:42:39 -0400

    criado a pasta contas e adicionado o package contas

---
+++ contas/contacorrente.go
@@ -0,0 +1,44 @@
+package Contas
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorSaque float64) string {
+	podeSacar := valorSaque > 0 && valorSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorSaque
+		return "Saque realizado com sucesso"
+	} else {
+		if valorSaque < 0 {
+			return "Valor informado é invalido"
+		}
+		return "Saldo insuficiente"
+	}
+
+}
+
+func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
+	if valorDeposito > 0 {
+		c.saldo += valorDeposito
+		return "Deposito realizado com sucesso", c.saldo
+	} else {
+		return "Valor informado é invlálido", c.saldo
+	}
+}
+
+func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
+
+	if valorTransferencia < c.saldo && valorTransferencia > 0 {
+		c.saldo -= valorTransferencia
+		contaDestino.Depositar(valorTransferencia)
+		return true
+	} else {
+		return false
+	}
+
+}
--- main.go
+++ main.go
@@ -1,53 +1,13 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorSaque float64) string {
-	podeSacar := valorSaque > 0 && valorSaque <= c.saldo
-
-	if podeSacar {
-		c.saldo -= valorSaque
-		return "Saque realizado com sucesso"
-	} else {
-		if valorSaque < 0 {
-			return "Valor informado é invalido"
-		}
-		return "Saldo insuficiente"
-	}
-
-}
-
-func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
-	if valorDeposito > 0 {
-		c.saldo += valorDeposito
-		return "Deposito realizado com sucesso", c.saldo
-	} else {
-		return "Valor informado é invlálido", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) bool {
-
-	if valorTransferencia < c.saldo && valorTransferencia > 0 {
-		c.saldo -= valorTransferencia
-		contaDestino.Depositar(valorTransferencia)
-		return true
-	} else {
-		return false
-	}
-
-}
+import (
+	"curso-golang-alura-oop/Contas",
+	"fmt"
+)
 
 func main() {
-	contaRafael := ContaCorrente{"Rafael Lima", 1189, 1234, 1000}
-	contaMitali := ContaCorrente{"MItali", 1189, 1234, 1000}
+	contaRafael := Contas.ContaCorrente{"Rafael Lima", 1189, 1234, 1000}
+	contaMitali := Contas.ContaCorrente{"MItali", 1189, 1234, 1000}
 
 	status := contaRafael.Transferir(300, &contaMitali)
 
